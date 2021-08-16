57db2125f0e343f6eecf8e2af9a551b7054b2d90
Shahid Manzoor Bhat <Shahid.Manzoor@gds.ey.com>
2021-08-16 15:51:12 +0530

    Add project files.

---
+++ DynamicConnectivity/QuickFind.cs
@@ -0,0 +1,31 @@
+﻿namespace DynamicConnectivity
+{
+    public class QuickFind
+    {
+        private int[] arr;
+        //Ctor sets value to each index to i
+        public QuickFind(int N)
+        {
+            arr = new int[N];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = i;
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return arr[p] == arr[q];
+        }
+
+        public void Union(int p, int q)
+        {
+            int pid = arr[p];
+            int qid = arr[q];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == pid)
+                    arr[i] = qid;
+            }
+        }
+
+    }
+}
---
+++ DynamicConnectivity/QuickUnion.cs
@@ -0,0 +1,32 @@
+﻿namespace DynamicConnectivity
+{
+    public class QuickUnion
+    {
+        private int[] arr;
+        public QuickUnion()
+        {
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = i;
+        }
+
+        public int Root(int i)
+        {
+            while (i != arr[i])
+                i = arr[i];
+            return i;
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return Root(p) == Root(q);
+        }
+
+        public void Union(int p, int q)
+        {
+            int i = Root(p);
+            int j = Root(q);
+            arr[i] = j;
+        }
+
+    }
+}
